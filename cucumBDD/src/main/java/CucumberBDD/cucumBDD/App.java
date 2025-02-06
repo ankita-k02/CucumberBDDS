1d4c29f38ef76dba603629f8651a50d943c8735f
ankita-k02 <itsankitak02@gmail.com>
2025-02-06 09:56:07 +0530

    first commit

---
+++ cucumBDD/src/main/java/CucumberBDD/cucumBDD/App.java
@@ -0,0 +1,13 @@
+package CucumberBDD.cucumBDD;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
